1b8cbb14a1b656435b0e0dbea9937e16e0225b07
ivanvl01 <ivan.v@kissmybutton.gr>
2023-02-10 10:33:45 +0200

    optimize imgs

--- clip/initParams.ts
+++ clip/initParams.ts
@@ -10,7 +10,6 @@ export default [
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/d/c/dc3729-003-phsrh000-1500.webp",
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/d/c/dc3729-101-phsrh000-1500.webp",
           ],
-          url: "https://www.e-tennis.gr/catalogsearch/result/?q=Nike+Revolution+6+Next+Nature",
         },
         {
           name: "Nike Sportswear",
@@ -19,7 +18,6 @@ export default [
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/b/v/bv6171-010-nike_sportswear_women_s_long-sleeve_t-shirt-8_1.webp",
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/b/v/bv6171-100-nike_sportswear_women_s_long-sleeve_t-shirt-0_1.webp",
           ],
-          url: "https://www.e-tennis.gr/catalogsearch/result/index/?q=Long-Sleeve+T-Shirt&sex=1443",
         },
         {
           name: "Nike Dri-fit Swoosh",
@@ -28,7 +26,6 @@ export default [
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/b/v/bv3630-010-nike-dri-fit-swoosh-women-s-sport-bra-black-white.webp",
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/b/v/bv3630-357-phsfm001-1500_1.webp",
           ],
-          url: "https://www.e-tennis.gr/catalogsearch/result/index/?clothes_types=571&q=Nike+Dri-fit+Swoosh",
         },
         {
           name: "Nike Pro 3",
@@ -37,7 +34,6 @@ export default [
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/c/z/cz9857-010-phsbm001-1500.webp",
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/c/z/cz9857-084-nike_pro_3_women_s_shorts-u_1.webp",
           ],
-          url: "https://www.e-tennis.gr/catalogsearch/result/index/?clothes_types=583&q=Nike+Pro+3",
         },
         {
           name: "Nike Pro Mid-Rise",
@@ -46,7 +42,6 @@ export default [
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/c/z/cz9779-451-phsfm001-1500.webp",
             "https://www.e-tennis.gr/pub/media/catalog/product/cache/f5f3da80ad7b670245aea7e970662954/c/z/cz9779-084-phsfm001-1500.webp",
           ],
-          url: "https://www.e-tennis.gr/catalogsearch/result/index/?clothes_types=841&q=Nike+Pro+Mid-Rise",
         },
       ],
     },
